45a669db6b1a2ee5caa692aaddf80f33a67eaf6a
Dipankar Das <dipankardas0115@gmail.com>
2022-10-24 08:58:19 +0530

    [Test,API](Civo,Local) Added isPresent and validNodeSize
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- src/api/civo/main_test.go
+++ src/api/civo/main_test.go
@@ -3,12 +3,14 @@ package civo
 import (
 	"fmt"
 	"github.com/kubesimplify/Kubesimpctl/src/api/payload"
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"os"
 	"os/exec"
 	"strings"
 	"testing"
 )
 
-//TODO: By Default fetchAPIKey() should return ""
 func TestFetchAPIKey(T *testing.T) {
 	apikey := fetchAPIKey()
 
@@ -17,7 +19,6 @@ func TestFetchAPIKey(T *testing.T) {
 	}
 }
 
-//TODO: Test isValidRegion()
 func TestIsValidRegion(T *testing.T) {
 	locationCombinations := map[string]bool{
 		"LOC":  false,
@@ -34,7 +35,16 @@ func TestIsValidRegion(T *testing.T) {
 	}
 }
 
-//TODO: Test getUserName()
+func TestIsValidNodeSize(T *testing.T) {
+	validSizes := []string{"g4s.kube.xsmall", "g4s.kube.small", "g4s.kube.medium", "g4s.kube.large", "g4p.kube.small", "g4p.kube.medium", "g4p.kube.large", "g4p.kube.xlarge", "g4c.kube.small", "g4c.kube.medium", "g4c.kube.large", "g4c.kube.xlarge", "g4m.kube.small", "g4m.kube.medium", "g4m.kube.large", "g4m.kube.xlarge"}
+	testData := validSizes[rand.Int()%len(validSizes)]
+	assert.Equalf(T, true, isValidSize(testData), "Returns False for valid size")
+
+	assert.Equalf(T, false, isValidSize("abcd"), "Returns True for invalid node size")
+	assert.Equalf(T, false, isValidSize("kube.small"), "Returns True for invalid node size")
+	assert.Equalf(T, false, isValidSize("g4s.k3s.small"), "Returns True for invalid node size")
+}
+
 func TestGetUserName(T *testing.T) {
 	usrCmd := exec.Command("whoami")
 
@@ -53,3 +63,35 @@ func TestGetUserName(T *testing.T) {
 //TODO: Test kubeconfigDeleter()
 
 //Testing of deleteClusterWithID() and DeleteCluster() and CreateCluster() [TODO Need to be done]
+
+func setup() {
+	err := os.MkdirAll(KUBECONFIG_PATH, 0750)
+	if err != nil {
+		return
+	}
+}
+
+func cleanup() {
+	//_ = DeleteCluster(clusterName)
+	err := os.RemoveAll(KUBECONFIG_PATH)
+	if err != nil {
+		return
+	}
+}
+
+func TestIsPresent(t *testing.T) {
+	setup()
+	present := isPresent("demo", "LON1")
+	assert.Equal(t, false, present, "with no clusters returns true! (false +ve)")
+	err := os.Mkdir(KUBECONFIG_PATH+"demo-LON1", 0755)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = os.Create(KUBECONFIG_PATH + "demo-LON1/info")
+	if err != nil {
+		t.Fatal(err)
+	}
+	present = isPresent("demo", "LON1")
+	cleanup()
+	assert.Equal(t, true, present, "Failed to detect the cluster (false -ve)")
+}
--- src/api/local/main_test.go
+++ src/api/local/main_test.go
@@ -1,6 +1,8 @@
 package local
 
 import (
+	"github.com/stretchr/testify/assert"
+	"os"
 	"strings"
 	"testing"
 )
@@ -49,4 +51,44 @@ nodes:
 	if strings.Compare(testRawConfig, string(rawConfig)) != 0 {
 		t.Fatalf("ERR node config didn't match")
 	}
+}
+
+func cleanup() {
+	//_ = DeleteCluster(clusterName)
+	err := os.RemoveAll(KUBECONFIG_PATH)
+	if err != nil {
+		return
+	}
+}
+
+func setup() {
+	err := os.MkdirAll(KUBECONFIG_PATH, 0750)
+	if err != nil {
+		return
+	}
+}
+
+func TestIsPresent(t *testing.T) {
+	setup()
+	present := isPresent("demo")
+	assert.Equal(t, false, present, "with no clusters returns true! (false +ve)")
+	//abcd := ClusterInfoInjecter("demo", 1)
+	//if err := CreateCluster(abcd); err != nil {
+	//	fmt.Println("[DEBUG] failed to create cluster: ", err.Error())
+	//	t.Fatalf("Unable to create cluster ENV not supported!")
+	//}
+
+	// create the folder
+	err := os.Mkdir(KUBECONFIG_PATH+"demo", 0755)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = os.Create(KUBECONFIG_PATH + "demo/info")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	present = isPresent("demo")
+	cleanup()
+	assert.Equal(t, true, present, "Failed to detect the cluster (false -ve)")
 }
