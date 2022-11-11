f505d76ad64409d32018b4d19c31e59fa006c3e5
Dipankar Das <dipankardas0115@gmail.com>
2022-11-11 22:34:31 +0530

    [HA Cluster](Civo) template folder added
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

---
+++ src/api/ha_civo/main.go
@@ -0,0 +1,8 @@
+package ha_civo
+
+// all the configs are present in .ksctl
+// want to save the config to ~/.ksctl/config/ha-civo/<Cluster Name> <Region>/*
+
+func demo() string {
+	return "Working"
+}
