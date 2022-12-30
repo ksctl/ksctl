c5a7294b996add2c8c14fac740cadde2d67839b2
Dipankar Das <dipankardas0115@gmail.com>
2022-12-30 16:18:46 +0530

    Added script to run testing for all providers
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

---
+++ api/test-api.ps1
@@ -0,0 +1,45 @@
+#Requires -Version 5
+
+$erroractionpreference = 'stop' # quit if anything goes wrong
+
+if (($PSVersionTable.PSVersion.Major) -lt 5) {
+  Write-Output "PowerShell 5 or later is required to run Datree."
+  Write-Output "Upgrade PowerShell: https://docs.microsoft.com/en-us/powershell/scripting/setup/installing-windows-powershell"
+  break
+}
+
+Write-Output "+-------------------------+"
+Write-Output "|   Testing (api/util)    |"
+Write-Output "+-------------------------+"
+
+Set-Location utils
+go test . -v && Set-Location -
+
+
+
+Write-Output "+-------------------------+"
+Write-Output "|   Testing (api/local)   |"
+Write-Output "+-------------------------+"
+
+Set-Location local
+go test . -v && Set-Location -
+
+
+
+Write-Output "+-------------------------+"
+Write-Output "|   Testing (api/civo)    |"
+Write-Output "+-------------------------+"
+
+Set-Location civo
+go test . -v && Set-Location -
+
+
+
+Write-Output "+-------------------------+"
+Write-Output "|  Testing (api/ha_civo)  |"
+Write-Output "+-------------------------+"
+
+Set-Location ha_civo
+go test . -v && Set-Location -
+
+
