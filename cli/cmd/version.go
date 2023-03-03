cd40fc8c04ec3141b53c31063a23a8159b214498
Anurag <kranurag7@linux.com>
2023-03-04 01:20:18 +0530

    add version and update goreleaser

---
+++ cli/cmd/version.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+const ksctl  = `
+ _             _   _ 
+| | _____  ___| |_| |
+| |/ / __|/ __| __| |
+|   <\__ \ (__| |_| |
+|_|\_\___/\___|\__|_|
+`
+var Version string = "dev"
+
+var Commit string 
+
+var BuildDate string
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of ksctl",
+	Aliases: []string{"v"},
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(ksctl)
+		fmt.Println("Version:", Version)
+		fmt.Println("Commit:", Commit)
+		fmt.Println("BuildDate:", BuildDate)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+                     
