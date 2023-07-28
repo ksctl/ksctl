df954dadb913769973054a5706b79c96657cff4c
Dipankar Das <dipankardas0115@gmail.com>
2023-07-28 16:38:32 +0530

    feat: controller to provider
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- api/controllers/cloud/cloud.go
+++ api/controllers/cloud/cloud.go
@@ -1,4 +1,22 @@
 package cloud
 
+import (
+	azure_pkg "github.com/kubesimplify/ksctl/api/provider/azure"
+	civo_pkg "github.com/kubesimplify/ksctl/api/provider/civo"
+	"github.com/kubesimplify/ksctl/api/resources"
+)
+
 // create a type for controlelr
 // which uses the controller.CreateHACluster(*KsctlClient) inside that
+
+func HydrateCloud(client *resources.KsctlClient) {
+	switch client.Metadata.Provider {
+	case "civo":
+		client.Cloud = civo_pkg.ReturnCivoStruct()
+	case "azure":
+		client.Cloud = azure_pkg.ReturnAzureStruct()
+	}
+}
+
+func CreateHACluster(client *resources.KsctlClient) {
+}
--- api/resources/controllers/controller.go
+++ api/resources/controllers/controller.go
@@ -3,13 +3,13 @@ package controllers
 import "github.com/kubesimplify/ksctl/api/resources"
 
 type Controller interface {
-	CreateManagedCluster(resources.KsctlClient)
-	DeleteManagedCluster(resources.KsctlClient)
+	CreateManagedCluster(*resources.KsctlClient)
+	DeleteManagedCluster(*resources.KsctlClient)
 
 	SwitchCluster()
 
 	GetCluster()
 
-	CreateHACluster(resources.KsctlClient)
-	DeleteHACluster(resources.KsctlClient)
+	CreateHACluster(*resources.KsctlClient)
+	DeleteHACluster(*resources.KsctlClient)
 }
