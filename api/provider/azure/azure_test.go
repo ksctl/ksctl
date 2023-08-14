32153d69cce04842854051600837d2d78eb2838b
Dipankar Das <dipankardas0115@gmail.com>
2023-08-14 23:19:54 +0530

    enhancement(azure-test): noWP(), noCP(), noDS()
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- api/provider/azure/azure_test.go
+++ api/provider/azure/azure_test.go
@@ -53,3 +53,81 @@ func TestGenPath(t *testing.T) {
 		utils.GetPath(utils.CLUSTER_PATH, "azure", "abcd"),
 		"genreatePath not compatable with utils.getpath()")
 }
+
+// Test for the Noof WP and setter and getter
+func TestNoOfControlPlane(t *testing.T) {
+	var no int
+	var err error
+	no, err = demoClient.Cloud.NoOfControlPlane(-1, false)
+	if no != -1 || err != nil {
+		t.Fatalf("Getter failed on unintalized controlplanes array got no: %d and err: %v", no, err)
+	}
+
+	_, err = demoClient.Cloud.NoOfControlPlane(1, true)
+	// it should return error
+	if err == nil {
+		t.Fatalf("setter should fail on when no < 3 controlplanes provided_no: %d", 1)
+	}
+
+	_, err = demoClient.Cloud.NoOfControlPlane(5, true)
+	// it should return error
+	if err != nil {
+		t.Fatalf("setter should not fail on when n >= 3 controlplanes err: %v", err)
+	}
+
+	no, err = demoClient.Cloud.NoOfControlPlane(-1, false)
+	if no != 5 {
+		t.Fatalf("Getter failed to get updated no of controlplanes array got no: %d and err: %v", no, err)
+	}
+}
+
+func TestNoOfDataStore(t *testing.T) {
+	var no int
+	var err error
+	no, err = demoClient.Cloud.NoOfDataStore(-1, false)
+	if no != -1 || err != nil {
+		t.Fatalf("Getter failed on unintalized datastore array got no: %d and err: %v", no, err)
+	}
+
+	_, err = demoClient.Cloud.NoOfDataStore(0, true)
+	// it should return error
+	if err == nil {
+		t.Fatalf("setter should fail on when no < 1 datastore provided_no: %d", 1)
+	}
+
+	_, err = demoClient.Cloud.NoOfDataStore(5, true)
+	// it should return error
+	if err != nil {
+		t.Fatalf("setter should not fail on when n >= 1 datastore err: %v", err)
+	}
+
+	no, err = demoClient.Cloud.NoOfDataStore(-1, false)
+	if no != 5 {
+		t.Fatalf("Getter failed to get updated no of datastore array got no: %d and err: %v", no, err)
+	}
+}
+
+func TestNoOfWorkerPlane(t *testing.T) {
+	var no int
+	var err error
+	no, err = demoClient.Cloud.NoOfWorkerPlane(demoClient.Storage, -1, false)
+	if no != -1 || err != nil {
+		t.Fatalf("Getter failed on unintalized workerplane array got no: %d and err: %v", no, err)
+	}
+
+	_, err = demoClient.Cloud.NoOfWorkerPlane(demoClient.Storage, 2, true)
+	// it shouldn't return err
+	if err != nil && !os.IsNotExist(err) {
+		t.Fatalf("setter should not fail on when no >= 0 workerplane provided_no: %d", 2)
+	}
+
+	_, err = demoClient.Cloud.NoOfWorkerPlane(demoClient.Storage, 2, true)
+	if err != nil {
+		t.Fatalf("setter should return nil when no changes happen workerplane err: %v", err)
+	}
+
+	no, err = demoClient.Cloud.NoOfWorkerPlane(demoClient.Storage, -1, false)
+	if no != 2 {
+		t.Fatalf("Getter failed to get updated no of workerplane array got no: %d and err: %v", no, err)
+	}
+}
