d6f6c7a2f146c986b1f7e07f519d506b3de452bc
Dipankar Das <dipankardas0115@gmail.com>
2023-08-17 18:08:58 +0530

    bug-fix(ha-civo,ha-azure): fixed the switch context feat(jenkinsfile): added
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- cli/cmd/addNodesHACivo.go
+++ cli/cmd/addNodesHACivo.go
@@ -34,11 +34,9 @@ ksctl create-cluster ha-civo add-nodes <arguments to civo cloud provider>
 
 		cli.Client.Metadata.IsHA = true
 
-		if !cmd.Flags().Lookup("approve").Changed {
-			if err := createApproval(); err != nil {
-				cli.Client.Storage.Logger().Err(err.Error())
-				os.Exit(1)
-			}
+		if err := createApproval(cmd.Flags().Lookup("approve").Changed); err != nil {
+			cli.Client.Storage.Logger().Err(err.Error())
+			os.Exit(1)
 		}
 
 		stat, err := controller.AddWorkerPlaneNode(&cli.Client)
