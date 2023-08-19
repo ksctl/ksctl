ee9001866192550cfb331c3fafc9d4e33a9936b4
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2023-08-19 12:03:34 +0530

    enhancement(docs): resolve issue #132 (#133)
    * task: migrated CONTRIBUTION to docs website
    
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>
    
    * task: ci pipeline diagram, new architecture diagrams
    
    also added the folder for cli reference
    
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>
    
    * improved docs website
    
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>
    
    ---------
    
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- website/src/components/HomepageFeatures/index.js
+++ website/src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Lightweight',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        it does not depends on any cli and hence its more is lightweight
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Fast',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        it creates cluster within matters of seconds
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Customize',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        customize all the aspects of cluster from size of nodes, k8s version, to pre-installed apps and many more
       </>
     ),
   },
