dc121bd6a6c8b089d9cd6b2208aebc17a8620c07
Dipankar Das <dipankardas0115@gmail.com>
2023-10-12 10:46:06 +0530

    added better postInstall for cilium
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- internal/k8sdistros/universal/cilium.go
+++ internal/k8sdistros/universal/cilium.go
@@ -9,7 +9,9 @@ func ciliumData() Application {
 		Version:    "1.14.2",
 		Metadata:   "Cilium is an open source, cloud native solution for providing, securing, and observing network connectivity between workloads, fueled by the revolutionary Kernel technology eBPF",
 		PostInstall: `
-TODO: Its blank
+Once all the components feel ready
+	cilium status
+to check the status of the cilium installed
 		`,
 		InstallType: InstallHelm,
 		HelmConfig: []WorkLoad{
