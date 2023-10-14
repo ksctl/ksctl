a5a19825850a089e9a59a1a2b67deab391326bbf
Dipankar Das <dipankardas0115@gmail.com>
2023-10-14 19:41:10 +0530

    fix: kube-prometheu helm install
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- internal/k8sdistros/universal/prometheusStack.go
+++ internal/k8sdistros/universal/prometheusStack.go
@@ -6,22 +6,22 @@ func prometheusStackData() Application {
 		Namespace:  "<Helm-Managed>",
 		Url:        "https://prometheus-community.github.io/helm-charts",
 		Maintainer: "Dipankar Das",
-		Version:    "1.16.1",
-		Metadata:   "Istio extends Kubernetes to establish a programmable, application-aware network using the powerful Envoy service proxy. Working with both Kubernetes and traditional workloads, Istio brings standard, universal traffic management, telemetry, and security to complex deployments.",
+		Version:    "51.7.0",
+		Metadata:   "kube-prometheus-stack collects Kubernetes manifests, Grafana dashboards, and Prometheus rules combined with documentation and scripts to provide easy to operate end-to-end Kubernetes cluster monitoring with Prometheus using the Prometheus Operator.",
 		PostInstall: `
-TODO: Its blank
+credential for accessing grafana
+	username: admin
+	password: kube-operator
+Done
 		`,
 		InstallType: InstallHelm,
 		HelmConfig: []WorkLoad{
 			WorkLoad{
 				chartName:   "prometheus-community/kube-prometheus-stack",
-				chartVer:    "0.68.0",
+				chartVer:    "51.7.0",
 				releaseName: "kube-prometheus-stack",
-				// namespace:   "istio-system",
-				// createns: true,
-				// args: map[string]interface{}{
-				// 	"defaultRevision": "default",
-				// },
+				namespace:   "monitoring",
+				createns:    true,
 			},
 		},
 	}
