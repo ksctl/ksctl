1b5184d1414d0909608c2495078a42f1a281a59c
horiodino <holiodin@gmail.com>
2023-10-27 22:25:38 +0530

    added: create-ssh for instances

--- internal/cloudproviders/aws/ssh.go
+++ internal/cloudproviders/aws/ssh.go
@@ -1,8 +1,13 @@
 package aws
 
 import (
+	"context"
 	"fmt"
+	"os"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"github.com/kubesimplify/ksctl/pkg/resources"
 )
 
@@ -12,6 +17,40 @@ func (obj *AwsProvider) CreateUploadSSHKeyPair(storage resources.StorageFactory)
 	obj.mxName.Unlock()
 
 	fmt.Println("AWS Create Upload SSH Key Pair")
+	keypairinput := &ec2.CreateKeyPairInput{
+		KeyName: aws.String("testkeypair"),
+		KeyType: types.KeyTypeRsa,
+		TagSpecifications: []types.TagSpecification{
+			{
+				ResourceType: types.ResourceTypeKeyPair,
+				Tags: []types.Tag{
+					{
+						Key:   aws.String("Name"),
+						Value: aws.String("testkeypair"),
+					},
+				},
+			},
+		},
+	}
+
+	response, err := obj.ec2Client().CreateKeyPair(context.Background(), keypairinput)
+	if err != nil {
+		return err
+	}
+	
+	file, err := os.Create("testkeypair.pem")
+	if err != nil {
+		return err
+	}
+
+	_, err = file.WriteString(*response.KeyMaterial)
+	if err != nil {
+		return err
+	}
+	file.Close()
+
+
+
 	return nil
 
 }
