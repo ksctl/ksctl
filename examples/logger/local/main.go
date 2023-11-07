1e20b9cc5aa062f8ea89ceaf22330774b8bcbe93
Dipankar Das <dipankardas0115@gmail.com>
2023-11-07 13:40:23 +0530

    added example for the logger to be used for saving in a file
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- examples/logger/local/main.go
+++ examples/logger/local/main.go
@@ -8,6 +8,13 @@ import (
 )
 
 func main() {
+	f, _ := os.OpenFile("/tmp/demo.log", os.O_CREATE|os.O_RDWR, 0755)
+	var logFile resources.LoggerFactory = logger.NewDefaultLogger(-1, f)
+	logFile.Print("Example", "key", "value")
+	logFile.Debug("HelloStdout")
+	logFile.Box("hello", "world!")
+
 	var log resources.LoggerFactory = logger.NewDefaultLogger(-1, os.Stdout)
 	log.Print("Example", "key", "value")
+	log.Debug("HelloStdout")
 }
