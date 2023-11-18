258df3b4595663adea0f4df8dbc79ea834786d58
Dipankar Das <dipankardas0115@gmail.com>
2023-11-18 13:04:51 +0530

    fixed the examples/
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- examples/manager/managed/useManager.go
+++ examples/manager/managed/useManager.go
@@ -1,30 +1,34 @@
 package main
 
 import (
+	"os"
+
 	control_pkg "github.com/kubesimplify/ksctl/pkg/controllers"
 	"github.com/kubesimplify/ksctl/pkg/resources"
 	"github.com/kubesimplify/ksctl/pkg/utils/consts"
 )
 
 func main() {
-	cli := new(resources.CobraCmd)
+	client := new(resources.KsctlClient)
 	controller := control_pkg.GenKsctlController()
 
-	cli.Client.Metadata.ClusterName = "example-cluster"
-	cli.Client.Metadata.StateLocation = consts.StoreLocal
-	cli.Client.Metadata.K8sDistro = consts.K8sK3s
+	client.Metadata.ClusterName = "example-cluster"
+	client.Metadata.StateLocation = consts.StoreLocal
+	client.Metadata.K8sDistro = consts.K8sK3s
 
-	cli.Client.Metadata.K8sVersion = "1.27.1"
+	client.Metadata.K8sVersion = "1.27.1"
 
-	cli.Client.Metadata.Provider = consts.CloudLocal
+	client.Metadata.Provider = consts.CloudLocal
 
-	cli.Client.Metadata.NoMP = 2
+	client.Metadata.NoMP = 2
+	client.Metadata.LogVerbosity = 0
+	client.Metadata.LogWritter = os.Stdout
 
-	if _, err := control_pkg.InitializeStorageFactory(&cli.Client, true); err != nil {
+	if err := control_pkg.InitializeStorageFactory(client); err != nil {
 		panic(err)
 	}
 
-	_, err := controller.CreateManagedCluster(&cli.Client)
+	err := controller.CreateManagedCluster(client)
 	if err != nil {
 		panic(err)
 	}
