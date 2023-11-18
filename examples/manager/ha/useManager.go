9f5decc7af1b4e7f80545af9ce2ff7b26228f01c
Dipankar Das <dipankardas0115@gmail.com>
2023-11-18 13:04:51 +0530

    fixed the examples/
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

--- examples/manager/ha/useManager.go
+++ examples/manager/ha/useManager.go
@@ -1,37 +1,42 @@
 package main
 
 import (
+	"os"
+
 	control_pkg "github.com/kubesimplify/ksctl/pkg/controllers"
 	"github.com/kubesimplify/ksctl/pkg/resources"
 	"github.com/kubesimplify/ksctl/pkg/utils/consts"
 )
 
 func main() {
-	cli := new(resources.CobraCmd)
+	client := new(resources.KsctlClient)
 	controller := control_pkg.GenKsctlController()
 
-	cli.Client.Metadata.ClusterName = "example-cluster"
-	cli.Client.Metadata.StateLocation = consts.StoreLocal
-	cli.Client.Metadata.K8sDistro = consts.K8sK3s
+	client.Metadata.ClusterName = "example-cluster"
+	client.Metadata.StateLocation = consts.StoreLocal
+	client.Metadata.K8sDistro = consts.K8sK3s
 
-	cli.Client.Metadata.K8sVersion = "1.27.1"
-	cli.Client.Metadata.IsHA = true
-	cli.Client.Metadata.Provider = consts.CloudAzure
+	client.Metadata.K8sVersion = "1.27.1"
+	client.Metadata.IsHA = true
+	client.Metadata.Provider = consts.CloudAzure
 
-	cli.Client.Metadata.NoWP = 2
-	cli.Client.Metadata.NoCP = 2
-	cli.Client.Metadata.NoDS = 1
+	client.Metadata.NoWP = 2
+	client.Metadata.NoCP = 2
+	client.Metadata.NoDS = 1
 
-	cli.Client.Metadata.WorkerPlaneNodeType = "Standard_F2s"
-	cli.Client.Metadata.ControlPlaneNodeType = "Standard_F2s"
-	cli.Client.Metadata.LoadBalancerNodeType = "Standard_F2s"
-	cli.Client.Metadata.DataStoreNodeType = "Standard_F2s"
+	client.Metadata.WorkerPlaneNodeType = "Standard_F2s"
+	client.Metadata.ControlPlaneNodeType = "Standard_F2s"
+	client.Metadata.LoadBalancerNodeType = "Standard_F2s"
+	client.Metadata.DataStoreNodeType = "Standard_F2s"
 
-	if _, err := control_pkg.InitializeStorageFactory(&cli.Client, true); err != nil {
+	client.Metadata.LogVerbosity = 0
+	client.Metadata.LogWritter = os.Stdout
+
+	if err := control_pkg.InitializeStorageFactory(client); err != nil {
 		panic(err)
 	}
 
-	_, err := controller.CreateHACluster(&cli.Client)
+	err := controller.CreateHACluster(client)
 	if err != nil {
 		panic(err)
 	}
