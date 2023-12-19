ca71d31c5a27994a03ee591d890fb4addf05b399
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2023-12-19 17:55:00 +0530

    added the mongodb poc work (#252)
    * added the mongodb poc work
    
    added the create, find and delete of records
    
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>
    
    * added interface with some ping tests
    
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>
    
    * done with interface design for mongodb
    
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>
    
    * added demonstration for the mongodb with storage
    
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>
    
    * refactor: added the mongodb Database client
    
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>
    
    * added the omitempty to the pointer fields
    
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>
    
    ---------
    
    Signed-off-by: Dipankar Das <dipankardas0115@gmail.com>

---
+++ poc/storage-mongo/types.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type MongoServer struct {
+	client         *mongo.Client
+	mongoURI       string
+	context        context.Context
+	databaseClient *mongo.Database
+}
+
+type Options struct {
+	Hostname string
+	Username string
+	Password string
+}
+
+type ConfigurationStore interface {
+	ListDatabases() ([]string, error)
+
+	Disconnect() error
+
+	Write(cloud string, data StorageDocument) error
+
+	ReadOne(cloud, region, clustername, clusterType string) (StorageDocument, error)
+
+	DeleteOne(cloud, region, clustername, clusterType string) error
+
+	IsPresent(cloud, region, clustername, clusterType string) bool
+
+	GetAllClusters(cloud string, filters bson.M) ([]StorageDocument, error)
+
+	DeleteAllInCloud(cloud string) error
+}
