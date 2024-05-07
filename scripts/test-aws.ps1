5e94183475b423deab0286f2127eda36061109c8
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2024-05-07 23:24:22 +0530

    feat: updated the codecov and other ci pipeline for new aws test suite
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

---
+++ scripts/test-aws.ps1
@@ -0,0 +1,18 @@
+#Requires -Version 5
+
+$erroractionpreference = 'stop' # quit if anything goes wrong
+
+if (($PSVersionTable.PSVersion.Major) -lt 5) {
+  Write-Output "PowerShell 5 or later is required to run Datree."
+  Write-Output "Upgrade PowerShell: https://docs.microsoft.com/en-us/powershell/scripting/setup/installing-windows-powershell"
+  break
+}
+
+Set-Location .\..\internal\
+
+Write-Output "--------------------------------------------"
+Write-Output "|   Testing (internal/cloudproviders/aws)"
+Write-Output "--------------------------------------------"
+
+Set-Location cloudproviders\aws
+go test . -v && Set-Location -
