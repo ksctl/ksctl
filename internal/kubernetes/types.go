81f6f8a2d65a8acf45015e883e0372c69d46e3da
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2024-06-24 12:59:34 +0530

    added initial modifiied application stack
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- internal/kubernetes/types.go
+++ internal/kubernetes/types.go
@@ -28,3 +28,31 @@ type HelmOptions struct {
 	createNamespace bool
 	args            map[string]interface{}
 }
+
+type HelmHandler struct {
+	createNamespace bool
+	namespace       string
+
+	postInstall string
+	metadata    string
+}
+
+type KubectlHandler struct {
+	namespace       string
+	createNamespace bool
+
+	chartVer    string
+	chartName   string
+	releaseName string
+	args        map[string]interface{}
+}
+
+type StackComponent struct {
+	helm    *HelmHandler
+	kubectl *KubectlHandler
+}
+
+type ApplicationStack struct {
+	// components it marks for the sequqncial dependency for each component
+	components []StackComponent
+}
