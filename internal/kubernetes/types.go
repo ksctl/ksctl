40dd906a890c1df6ad4fc49d7d31373b1905ff38
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2024-06-28 00:38:57 +0530

    added stack type to the ApplicationStack struct
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- internal/kubernetes/types.go
+++ internal/kubernetes/types.go
@@ -50,11 +50,19 @@ type KubectlHandler struct {
 	version     string
 }
 
-type StackComponentType uint
+type (
+	StackComponentType uint
+	StackType          string
+)
 
 const (
 	ComponentTypeHelm    StackComponentType = iota
 	ComponentTypeKubectl StackComponentType = iota
+)
+
+const (
+	StackTypeProduction StackType = "production"
+	StackTypeStandard   StackType = "standard"
 )
 
 type StackComponent struct {
@@ -63,10 +71,20 @@ type StackComponent struct {
 	handlerType StackComponentType
 }
 
+// ApplicationStack TODO(dipankar): need to think about version peneatration from the stack later to the crd level
+//
+//	Also need to think of taking some sport of the application ksctl provide from the src to some json file in ver control
+//	so that we can update that and no need of update of the logicial part
 type ApplicationStack struct {
 	// components it marks for the sequential dependency for each component
 	components []StackComponent
 
+	// StackType NOTE(dipankar):
+	//		if `prod` components are treated as a part of the entire stack
+	// Suitable for Production mode
+	//		else `std` components are treated as loosly grouped or accomulated standard installation without keeping in mind whether the application are of similar nature or deps
+	// Suitable for Development mode
+	StackType  StackType
 	Maintainer string
 
 	StackNameID string
