71bee419ed3f22298f02fa4bb3bfc3f2e4c89cf7
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2024-07-09 21:24:20 +0530

    note(versions)!: changed the lates ksctl components
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- ksctl-components/manifests/versions.go
+++ ksctl-components/manifests/versions.go
@@ -1,13 +1,12 @@
 // Dont edit it unless you know what you are doing
 // This file contains the version of the ksctl agent and ksctl state import
 // used to provide the ksctl core which version to fetch based on ksctl branch
+// KsctlApplicationStackBranchOrTagName CAUTION: make sure it is restored to `latest`
+// Once the dev is done
 package manifests
 
 const (
-	KsctlAgentAppVersion       = "latest"
-	KsctlStateImportAppVersion = "latest"
-
-	// KsctlApplicationStackBranchOrTagName CAUTION: make sure it is restored to `latest`
-	// Once the dev is done
-	KsctlApplicationStackBranchOrTagName = "latest"
+	KsctlAgentAppVersion                 = "feat-ksctl-application-hybrid-client"
+	KsctlStateImportAppVersion           = "feat-ksctl-application-hybrid-client"
+	KsctlApplicationStackBranchOrTagName = "feat-ksctl-application-hybrid-client"
 )
