cebce9498800d849608a076df5c35bb33270003d
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2024-07-19 15:53:38 +0530

    added helm overidings for cilium cni
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- internal/kubernetes/components/cilium.go
+++ internal/kubernetes/components/cilium.go
@@ -5,15 +5,19 @@ import (
 	"github.com/ksctl/ksctl/pkg/helpers/utilities"
 )
 
-func getCiliumComponentOverridings(p metadata.ComponentOverriding) (version *string) {
+func getCiliumComponentOverridings(p metadata.ComponentOverriding) (version *string, ciliumChartOverridings map[string]any) {
+	ciliumChartOverridings = nil // By default it is nil
+
 	if p == nil {
-		return nil
+		return nil, nil
 	}
 
 	for k, v := range p {
 		switch k {
 		case "version":
 			version = utilities.Ptr(v.(string))
+		case "ciliumChartOverridings":
+			ciliumChartOverridings = v.(map[string]any)
 		}
 	}
 	return
@@ -21,11 +25,20 @@ func getCiliumComponentOverridings(p metadata.ComponentOverriding) (version *str
 
 func CiliumStandardComponent(params metadata.ComponentOverriding) metadata.StackComponent {
 	var (
-		version = "latest"
+		version                = "latest"
+		ciliumChartOverridings = map[string]any{}
 	)
-	_version := getCiliumComponentOverridings(params)
+
+	_version, _ciliumChartOverridings := getCiliumComponentOverridings(params)
+
 	if _version != nil {
 		version = *_version
+	}
+
+	if _ciliumChartOverridings != nil {
+		ciliumChartOverridings = _ciliumChartOverridings
+	} else {
+		ciliumChartOverridings = nil
 	}
 
 	return metadata.StackComponent{
@@ -39,7 +52,7 @@ func CiliumStandardComponent(params metadata.ComponentOverriding) metadata.Stack
 					ReleaseName:     "cilium",
 					Namespace:       "kube-system",
 					CreateNamespace: false,
-					Args:            nil,
+					Args:            ciliumChartOverridings,
 				},
 			},
 		},
