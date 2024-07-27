30708cc3f15bff1e5a7fbd31c6d5e53964c3bfb1
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2024-07-27 15:51:06 +0530

    added new image tag for the ksctl state importor
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- ksctl-components/manifests/versions.go
+++ ksctl-components/manifests/versions.go
@@ -2,7 +2,7 @@ package manifests
 
 const (
 	KsctlAgentAppVersion       = "latest"
-	KsctlStateImportAppVersion = "latest"
+	KsctlStateImportAppVersion = "12313123"
 
 	// KsctlApplicationStackBranchOrTagName CAUTION: make sure it is restored to `latest`
 	// Once the dev is done
