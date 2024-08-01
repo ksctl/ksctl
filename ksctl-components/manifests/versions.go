96f7fa6e00d4c17a497abbdc276353d6538654bb
Dipankar Das <65275144+dipankardas011@users.noreply.github.com>
2024-08-01 15:27:14 +0000

    [skip ci]!: Restored the Development ksctl-component versions
    Signed-off-by: Dipankar Das <65275144+dipankardas011@users.noreply.github.com>

--- ksctl-components/manifests/versions.go
+++ ksctl-components/manifests/versions.go
@@ -1,10 +1,10 @@
 package manifests
 
 const (
-	KsctlAgentAppVersion       = "v1.2.5"
-	KsctlStateImportAppVersion = "v1.2.5"
+	KsctlAgentAppVersion       = "latest"
+	KsctlStateImportAppVersion = "latest"
 
 	// KsctlApplicationStackBranchOrTagName CAUTION: make sure it is restored to `latest`
 	// Once the dev is done
-	KsctlApplicationStackBranchOrTagName = "v1.2.5"
+	KsctlApplicationStackBranchOrTagName = "latest"
 )
